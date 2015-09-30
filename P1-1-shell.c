b4206b3dd8ae48815d3d1ca588c32a929cefdd5e
Bill <jpratt9@gatech.edu>
2015-09-29 21:51:46 -0400

    first commit

---
+++ P1-1-shell.c
@@ -0,0 +1,52 @@
+/*  <your name goes here>
+
+This program finds George (possibly incognito) in a crowd. */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+   int	             CrowdInts[1024];
+   int	             NumInts, Location=0;
+   int               Load_Mem(char *, int *);
+
+   if (argc != 2) {
+     printf("usage: ./P1-1 valuefile\n");
+     exit(1);
+   }
+   NumInts = Load_Mem(argv[1], CrowdInts);
+   if (NumInts != 1024) {
+      printf("valuefiles must contain 1024 entries\n");
+      exit(1);
+   }
+
+   /* your code goes here. */
+
+   printf("The rightmost pixel at the top of George's hat is located at: %4d.\n", Location);
+   exit(0);
+}
+
+/* This routine loads in up to 1024 newline delimited integers from
+a named file in the local directory. The values are placed in the
+passed integer array. The number of input integers is returned. */
+
+int Load_Mem(char *InputFileName, int IntArray[]) {
+   int	N, Addr, Value, NumVals;
+   FILE	*FP;
+
+   FP = fopen(InputFileName, "r");
+   if (FP == NULL) {
+      printf("%s could not be opened; check the filename\n", InputFileName);
+      return 0;
+   } else {
+      for (N=0; N < 1024; N++) {
+         NumVals = fscanf(FP, "%d: %d", &Addr, &Value);
+         if (NumVals == 2)
+            IntArray[N] = Value;
+         else
+            break;
+      }
+      fclose(FP);
+      return N;
+   }
+}
