bc84244c439d0df1d5344cec33ab476ddd8d15bd
Bill <jpratt9@gatech.edu>
2015-10-05 07:53:25 -0400

    C working, needs subroutines

--- P1-1.c
+++ P1-1.c
@@ -26,56 +26,56 @@ int main(int argc, char *argv[]) {
    int row, col;
    int found;
    for(i = 0; i < 1024; i++) {
-      found = false;
       //once row > 52 or column >
       row = i / 16;
       col = i % 16;
       // row and col max values calculated to prevent going out of bounds 
       if (row < 53 && col < 14) {
-         for (j = 3; j >= 0; j--) {
-
+         for (j = 0; j < 4; j++) {
             // shift line to pixel of interest 
             tmp = CrowdInts[i] >> (8 * j);
             // remove everything except pixel of interest
             tmp = tmp & 0xF;
-
+            
             // only found if our pixel is blue or black
             found = (tmp == 3) || (tmp == 8);
-
-            // check if pixel in (,) is white:
-            // the i-shift is precalculated given all possible values of j and 
-            // where the white pixel is in relation to the blue/black pixel 
-            tmp = CrowdInts[i + 16 + (j == 0)];
-            // the j-shift is precalculated given all possible values of j and 
-            // where the white pixel will be inside the "line" given the value 
-            // of j
-            tmp = tmp >> ((j + 3) % 4);
-            // now again we get rid of everything but the pixel of interest
-            tmp = tmp & 0xF;
-
-            // "found" now includes whether this other pixel was white
-            found = found && (tmp == 1);
-            
             if (found) {
-               // apply same method as before to see if skin is yellow
-               tmp = CrowdInts[i + (6 * 16) - (j == 0)];
-               tmp = tmp >> (8 * 3 * (j < 3));
-               tmp = tmp & 0xF;
-               found = found && (tmp == 5);
-
-               // hat is blue
                if (tmp == 3) {
-                  // check to make sure he's not wearing any glasses
+                  // check if pixel in stripe is white:
+                  // the i-shift is precalculated given all possible values of j and 
+                  // where the white pixel is in relation to the blue/black pixel 
+                  tmp = CrowdInts[i + 16 + (j == 3)];
+
+                  // the j-shift is precalculated given all possible values of j and 
+                  // where the white pixel will be inside the "line" given the value 
+                  // of j
+                  tmp = tmp >> (8 * ((j + 1) % 4));
+
+                  // now again we get rid of everything but the pixel of interest
+                  tmp = tmp & 0xF;
+
+                  // "found" now includes whether this other pixel was white
+                  found = found && (tmp == 1);
+
+                  // use the same process as before to make sure we're on the 
+                  // left-most pixel of top of hat
+                  tmp = CrowdInts[i - (j == 0)];
+                  tmp = tmp >> (8 * ((j + 3) % 4));
+                  tmp = tmp & 0xF;
+                  found = found && (tmp != 3);
+
+                  // this step will confirm we're in the top-left of the hat by
+                  // checking for skin in the top left of his face
                   tmp = CrowdInts[i + (5 * 16) - (j == 0)];
-                  tmp = tmp >> (8 * 3 * (j < 3));
+                  tmp = tmp >> (8 * ((j + 3) % 4));
                   tmp = tmp & 0xF;
                   found = found && (tmp == 5);
 
-                  // eyes are green
+                  // check that eyes are green
                   tmp = CrowdInts[i + (5 * 16)];
                   tmp = tmp >> (8 * j);
                   tmp = tmp & 0xF;
-                  found = found && (tmp == 7);
+                  found = found && (tmp == 7); 
 
                   // red smile
                   tmp = CrowdInts[i + (7 * 16)];
@@ -84,34 +84,53 @@ int main(int argc, char *argv[]) {
                   found = found && (tmp == 2);
 
                   // blue shirt
-                  tmp = CrowdInts[i +_(11 * 16)];
-                  tmp = tmp & 0xF;
-                  found = found && (tmp == 3);
-
-               }
-
-               // hat is black 
-               else if (tmp == 8) {
-                  // check to make sure his glasses are blue
-                  tmp = CrowdInts[i + (5 * 16) - (j == 0)];
-                  tmp = tmp >> (8 * 3 * (j < 3));
-                  tmp = tmp & 0xF;
-                  found = found && (tmp == 3);
-
-                  // black mustache
-                  tmp = CrowdInts[i + (7 * 16)];
+                  tmp = CrowdInts[i + (11 * 16)];
                   tmp = tmp >> (8 * j);
                   tmp = tmp & 0xF;
-                  found = found && (tmp == 2);
+                  found = found && (tmp == 3);
+                  
+               }
+               else if (tmp == 8) {
+                  // check if stripe is white 
+                  tmp = CrowdInts[i + 16 + (j == 3)];
+                  tmp = tmp >> (8 * ((j + 1) % 4));
+                  tmp = tmp & 0xF;
+                  found = found && (tmp == 1);
+
+                  // use the same process as before to make sure we're on the 
+                  // left-most pixel of top of hat
+                  tmp = CrowdInts[i - (j == 0)];
+                  tmp = tmp >> (8 * ((j + 3) % 4));
+                  tmp = tmp & 0xF;
+                  found = found && (tmp != 8);
+
+                  // this step will confirm we're in the top-left of the hat by
+                  // checking for skin in the top left of his face
+                  tmp = CrowdInts[i + (6 * 16) - (j == 0)];
+                  tmp = tmp >> (8 * ((j + 3) % 4));
+                  tmp = tmp & 0xF;
+                  found = found && (tmp == 5);
+
+                  // check that glasses are blue
+                  tmp = CrowdInts[i + (5 * 16)];
+                  tmp = tmp >> (8 * j);
+                  tmp = tmp & 0xF;
+                  found = found && (tmp == 3); 
+
+                  // black moustache
+                  tmp = CrowdInts[i + (7 * 16) + j / 2];
+                  tmp = tmp >> (8 * ((j + 2) % 4));
+                  tmp = tmp & 0xF;
+                  found = found && (tmp == 8);
 
                   // black shirt
-                  tmp = CrowdInts[i +_(11 * 16)];
+                  tmp = CrowdInts[i + (11 * 16)];
+                  tmp = tmp >> (8 * j);
                   tmp = tmp & 0xF;
                   found = found && (tmp == 8);
                }
                if (found) {
-                  Location = (4 * i) + (6 - j);
-                  break;
+                     Location = 4 * i + j + 3;
                }
             }
          }
