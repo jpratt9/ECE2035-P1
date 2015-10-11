89a2696e6ef02b1a21a09760f4efb5a91f6ab886
John Pratt <jpratt9@gatech.edu>
2015-10-11 07:42:42 -0700

    cutting down MIPS pixel finding

--- P1-2.asm
+++ P1-2.asm
@@ -18,41 +18,56 @@ FindGeorge: addi  $1, $0, Array  # point to array base
             addi  $1, $0, -4     # i = -1
             addi  $2, $0, 64     # (constant) = 16 (it's a waste of dynamic
                                  # calls to do this every loop iteration)
-            addi  $3, $0, 8      # (constant2) = 8 (same reason as above, will be used when calling srlv)
-            addi  $4, $0, 4      # (constant3) = 4 (used when shifting inside array element)
+            addi  $3, $0, 4      # (constant3) = 4 (used when shifting inside array element)
 
             # note - we precalculate the max value for i's of interest
             # this is based on the fact that faces can't hang off the picture
 Outer:      addi  $1, $1, 4      # i++
-            slti  $5, $1, 3384   # i < 846 (precalculated - explained above loop header)
-            beq   $5, $0, End    # if i >= 847, end loop
+            slti  $4, $1, 3384   # i < 846 (precalculated - explained above loop header)
+            beq   $4, $0, End    # if i >= 847, end loop
             
             # check col
             div   $1, $2
-            mfhi  $5             # col = i % 16
-            slti  $5, $5, 14     # col < 14
-            beq   $5, $0, Outer  # our i is off, so we have to skip this i
+            mfhi  $4             # col = i % 16
+            slti  $4, $4, 14     # col < 14
+            beq   $4, $0, Outer  # our i is off, so we have to skip this i
 
-            addi  $5, $0, -1     # j = -1
-Inner:      addi  $5, $5, 1      # j++
-            slti  $6, $5, 4      # j < 4
-            beq   $6, $0, Outer  # if j >= 4, go back to outer loop   
+            addi  $4, $0, -1     # j = -1
+Inner:      addi  $4, $4, 1      # j++
+            slti  $5, $4, 4      # j < 4
+            beq   $5, $0, Outer  # if j >= 4, go back to outer loop   
                   
             # first we'll make sure we're on the top-left of a blue or black hat      
             # get ready to call Shift (this isn't the best example of how Shift works - refer to where we test if the hat stripe is correct)
-            addi  $6, $0, 0      # how many elements in CrowdInts we have to shift
-            addi  $7, $0, 4      # k - used in modulus (how many 'pixels' in CrowdInts to shift)
+            addi  $5, $0, 0      # how many elements in CrowdInts we have to shift
+            addi  $6, $0, 4      # k - used in modulus (how many 'pixels' in CrowdInts to shift)
             jal   Shift
+
+            addi  $6, $0, 8      
+            beq   $6, $5, Black  # if tmp is black, go to black code
+            addi  $6, $0, 3
+            bne   $6, $5, Inner  # if tmp is neither black nor blue, skip this pixel
+
+            # begin blue code
+Blue:       # START HERE
+            jal   Shift
+
+
+
+Black:      
+
+
 
             # other code for pixels to test
 
-Shift:      add   $6, $1, $6     # place = i + shift
-            lw    $6, Array($6)  # tmp = CrowdInts(place)
-            add   $7, $7, $5     # j-shift = j + k
-            div   $7, $4
-            mfhi  $7             # j-shift = (j + k) % 4
-            mul   $s, $t
-            
+Shift:      add   $5, $1, $5     # place = i + shift
+            lw    $5, Array($5)  # tmp = CrowdInts(place)
+            add   $6, $6, $     # j-shift = j + k
+            div   $6, $3
+            mfhi  $6             # j-shift = (j + k) % 4
+            sll   $6, $6, 3      # j-shift = 8 * ((j + k) % 4))
+            srlv  $5, $5, $6     # tmp = CrowdInts(place) >> j-shift
+            andi  $5, $5, 0xF    # (remove everything except pixel of interest)
             jr    $31            # return to subroutine caller
 
             
