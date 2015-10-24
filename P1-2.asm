665f6c1b6c6bf67214330e291ca42067cacceb43
Bill <jpratt9@gatech.edu>
2015-10-24 12:26:25 -0400

    Refactoring MIPS to reduce instructions.  Now manually calculates row & col, then does index based on these.

--- P1-2.asm
+++ P1-2.asm
@@ -1,4 +1,4 @@
-   #    Find George Possibly Incognito in a Crowd
+#    Find George Possibly Incognito in a Crowd
 #
 #
 # This routine finds an exact match of George's face (possibly incognito)
@@ -7,53 +7,37 @@
 # 10-11-2015                               John Pratt
 
 .data
-Array:  .alloc 1024
+Array:      .alloc 1024
 
 .text
 
-FindGeorge: addi  $1, $0, Array  # point to array base
-            swi   588            # generate crowd
+FindGeorge: addi  $1, $0, Array     # point to array base
+            swi   588               # generate crowd
 
-            lbu   $2, 0($1)
-            lbu   $3, 1($1)
-            lbu   $4, 2($1)
-            lbu   $5, 3($1)
-
-            # your code goes here
-            addi  $1, $1, -1     # i = -1
-
-            # note - we precalculate the max value for i's of interest
-            # this is based on the fact that faces can't hang off the picture
-Outer:      addi  $1, $1, 1      # i++
-            slti  $4, $1, 3384   # i < 846 (precalculated - explained above loop header)
-            beq   $4, $0, End    # if i >= 847, end loop
-            
-            # check col
-            #rem   $4, $1, $2     # col = i % 16     
-            slti  $4, $4, 14     # col < 14
-            beq   $4, $0, Outer  # our i is off, so we have to skip this i
-
-            
-
-            
-
-End:        jr    $31            # return to caller
+            # We're going to move down columns rather than across rows
+            # That way, if we find a face that isn't right we can skip down a 
+            # few rows, and when we get to the bottom, we can skip to the next
+            # column.
+            addi  $1, $0, -1        # col
+ColLoop:    addi  $1, $1, 1         # col++
+            addi  $2, $0, -1        # row
+RowLoop:    addi  $2, $2, 1         # row++
+            slti  $5, $2, 58        # tmp = (row < 58)
+            beq   $5, $0, ColLoop   # if (row >= 58), skip to next column
+            sll   $3, $
 
 
-
-
-
-
+      
             ### The following are temporary (replace them).
             ### They are included to show how to use swi 552 and 553.
-            addi  $2, $0, 160    # mark the 160th pixel
-            swi   552            # with this swi
+            addi  $2, $0, 160       # mark the 160th pixel
+            swi   552               # with this swi
+   
+            addi  $2, $0, 161       # mark the 161th pixel
+            swi   552               # with this swi
 
-            addi  $2, $0, 161    # mark the 161th pixel
-            swi   552            # with this swi
-
-            addi  $2, $0, 300    # always guess the 300th pixel
+            addi    $2, $0, 300     # always guess the 300th pixel
             ### The above instructions are temporary (replace them).
 
-            swi   553            # submit answer and check
-                        
+End:        swi   553               # submit answer and check
+            jr    $31               # return to caller
